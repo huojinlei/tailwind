edac70801a90ec8fd90f77a7b06bb3e5ae685eff
huojinlei <15829718950@163.com>
2022-09-18 19:31:22 +0800

    [+]init project

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="app" class="h-full w-full">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="w-full h-full flex bg-gray-100">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde similique a, ipsum alias dolorem eum quasi qui tempora. Delectus nobis ea quae ipsa laudantium id quidem rem veritatis! Culpa, accusamus?
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
